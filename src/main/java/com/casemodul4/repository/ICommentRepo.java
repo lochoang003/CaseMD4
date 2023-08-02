d40072797b0d69c66827a158dea621c22f3bf61b
lochoang003 <129358033+lochoang003@users.noreply.github.com>
2023-08-02 20:15:41 +0700

    a

--- src/main/java/com/casemodul4/repository/ICommentRepo.java
+++ src/main/java/com/casemodul4/repository/ICommentRepo.java
@@ -1,7 +1,6 @@
 package com.casemodul4.repository;
 
 import com.casemodul4.model.Comment;
-import com.casemodul4.model.dto.CommentDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
